2149bc4bd5c53e0ba313ab1545f7af7e01e28bb0
arjunkhode <arjunkhode@gmail.com>
2017-05-07 18:41:13 -0700

    Added person scaffold, but css is not working

---
+++ src/components/person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Person extends React.Component{
+	render(){
+		return(
+		<div className="person">
+			Hello I am a person
+		</div>
+		);
+	}
+}
--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import HeroImage from './components/hero-image';
 import People from './components/people';
+import Person from './components/person';
 
 export default(
 	<Route path="/" component={App} >
 		<IndexRoute component={HeroImage} />
 		<Route path="people" component={People} />
+		<Route path="people/:personid" component={Person} />
 	</Route>
 );
